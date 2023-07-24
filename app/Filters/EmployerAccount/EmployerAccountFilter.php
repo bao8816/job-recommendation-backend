8234d8019fd236588a492719037fb29f7fdb26e2
bao8816 <nile1087@gmail.com>
2023-07-24 18:00:42 +0700

    filter by username in employer account

--- app/Filters/EmployerAccount/EmployerAccountFilter.php
+++ app/Filters/EmployerAccount/EmployerAccountFilter.php
@@ -3,10 +3,12 @@
 namespace App\Filters\EmployerAccount;
 
 use App\Filters\AbstractFilter;
+use App\Filters\CommonFilter\UsernameFilter;
 
 class EmployerAccountFilter extends AbstractFilter
 {
     protected $filters = [
         'company_id' => CompanyFilter::class,
+        'username' => UsernameFilter::class,
     ];
 }
