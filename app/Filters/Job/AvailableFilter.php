2c50aa48b49da3d8d6ca327d06e1e6190617d0ca
bao8816 <nile1087@gmail.com>
2023-08-15 15:30:09 +0700

    modify filter available job

--- app/Filters/Job/AvailableFilter.php
+++ app/Filters/Job/AvailableFilter.php
@@ -7,9 +7,10 @@ class AvailableFilter
     public function filter($builder, $value)
     {
         if ($value == 1) {
-            return $builder->where('deadline', '>=', now());
+            return $builder->where('deadline', '>=', now())
+                ->orWhere('status', 'Đang tuyển');
         }
 
-        return $builder->where('deadline', '<', now());
+        return $builder->where('deadline', '<', now())->orWhere('status', 'Ngừng tuyển');
     }
 }
